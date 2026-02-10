962861e24256919c563e8ed154587c809a101948
PLH <1772045545@qq.com>
2026-02-10 23:05:22 +0800

    工具完善

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning>
-      <body className="min-h-screen bg-background font-sans antialiased">
+      <body className="min-h-screen bg-background font-sans antialiased" suppressHydrationWarning>
         <Analytics />
         <ThemeProvider
           attribute="class"
